1b17a550f5044977935b7594355dbac86d588371
olgame2027 <olgame2027@gmail.com>
2025-10-08 09:37:40 +0300

    add compute

--- compute.cpp
+++ compute.cpp
@@ -1,6 +1,11 @@
 #include "compute.h"
 
 double computeSum(int n) {
-    // TODO: реализовать
-    return 0.0;
+    double s = 0.0;
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+        s += cos(sum);
+    }
+    return s;
 }
